4c2fe27d9990adead393a5ee8338450df57b72cf
Igor <Rogovski_igor@mail.ru>
2022-08-05 18:07:41 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+ 
+console.log(4 + +'5');
+console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(4*2 !== 8);
+
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && !isClose);
